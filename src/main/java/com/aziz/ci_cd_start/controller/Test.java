fbf25bbcbd763657a46d0820ef2b55a3195404ea
Aziz <niftalizadeaziz@gmail.com>
2026-04-02 08:07:13 +0400

    added integration on server and remove cd root script and boy photos

--- src/main/java/com/aziz/ci_cd_start/controller/Test.java
+++ src/main/java/com/aziz/ci_cd_start/controller/Test.java
@@ -2,27 +2,44 @@ package com.aziz.ci_cd_start.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class Test {
     @GetMapping("/test")
-    public String test(){
-        return "Hello world v1 added integration on server ahahhahaaahhahhahahahah yahoooooooooo!!!";
+    public String test() {
+        return "Hello world v1 added integration on server ahahhahaaahhahhahahahah yahoooooooooo!!!  oqlannnnnn";
     }
+
 
     @GetMapping("/oglan")
-    public String redirectToPhoto() {
-        return "redirect:/IMG_9620.JPG";
-    }
+    public ResponseEntity<byte[]> photo(@RequestParam String ad) throws IOException {
 
+        ClassPathResource imgFile ;
+        switch (ad) {
+            case "Aziz" -> imgFile = new ClassPathResource("static/IMG_9620.JPG");
+            case "Ismayil" -> imgFile = new ClassPathResource("static/img.png");
+            case "Nemet" -> imgFile = new ClassPathResource("static/img_1.png");
+            default -> {
+                imgFile = new ClassPathResource("static/hahaha.jpg");
+                byte[] bytes = imgFile.getInputStream().readAllBytes();
+                return ResponseEntity.ok()
+                        .header("Content-Type", "image/jpeg").body(bytes);
+            }
+        }
+        byte[] bytes = imgFile.getInputStream().readAllBytes();
+
+
+        return ResponseEntity.ok()
+                .header("Content-Type", "image/jpeg")
+                .body(bytes);
+    }
 }
